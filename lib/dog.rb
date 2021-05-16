382ab60147e97251714550992278247846792aaa
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-05-16 21:10:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # Add your code here
+
+class Dog 
+  @@all = []
+  
+  attr_accessor :name
+  
+  initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end 
+  
+end
